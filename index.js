dd98ba9a0d8b79713b3b1a8294dd089ea50f2716
harsh-si14 <harsh.vardhan1_cs20@gla.ac.in>
2024-05-16 12:58:42 +0530

    nine

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     methods: "GET,POST,PUT,DELETE",
     credentials: true
 }));
